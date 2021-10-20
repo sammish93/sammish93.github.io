a9930221f6c2e29f02222870ff3f022c266639f9
sammish93 <samd93@hotmail.co.uk>
2021-10-20 08:49:11 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+function mobileMenu() {
+    var x = document.getElementById("ham");
+
+    if(x.style.display === "block") {
+      x.style.display = "none"
+
+    }
+    else {
+      x.style.display = "block";
+    }
+  }
+
+  function mobileMenuSub() {
+    var x = document.getElementById("ham-sub");
+
+    if(x.style.display === "block") {
+      x.style.display = "none"
+
+    }
+    else {
+      x.style.display = "block"
+    }
+  }
+
+  function mobileMenuSub2() {
+    var x = document.getElementById("ham-sub2");
+
+    if(x.style.display === "block") {
+      x.style.display = "none"
+
+    }
+    else {
+      x.style.display = "block";
+    }
+  }
+  
+  function mobileMenuSub3() {
+    var x = document.getElementById("ham-sub3");
+
+    if(x.style.display === "block") {
+      x.style.display = "none"
+
+    }
+    else {
+      x.style.display = "block";
+    }
+  }
